32a47669acb781f8dfca1c9061e0d56ebea4e75d
Atharva Sindwani <144984824+atharvasindwani23@users.noreply.github.com>
2026-07-11 21:58:56 -0700

    Update: is-anagram - submission-1

--- Data Structures & Algorithms/is-anagram/submission-1.cpp
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return (s == t);
+        //sort strings and check if they're the same.
+        //use a map and check if the frequency of the characters is the same in each of the cases
+        unordered_map<char,int> freq_s;
+        unordered_map<char, int> freq_t;
+
+        for (char ch : s) {
+            freq_s[ch]++;
+        }
+        for (char ch : t) {
+            freq_t[ch]++;
+        }
+        if (freq_s.size() != freq_t.size()) {
+            return false;
+        }
+        for (auto x = freq_s.begin(); x != freq_s.end(); x++) {
+            if (x->second != freq_t[x->first]) {
+                return false;
+            }
+        }
+        return true;
     }
 };
