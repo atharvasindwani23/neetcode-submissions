165e29ad21aad6639131d01fd8588260916a08c4
Atharva Sindwani <144984824+atharvasindwani23@users.noreply.github.com>
2026-07-13 17:18:33 -0700

    Update: anagram-groups - submission-0

--- Data Structures & Algorithms/anagram-groups/submission-0.cpp
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        std::map<string, vector<string>> anagrams;
+        vector<vector<string>> result;
+
+        unordered_map<string, vector<string>> anagrams_;
+
         for (string str : strs) {
-            string copy = str;
-            sort(copy.begin(), copy.end());
-            anagrams[copy].push_back(str);
+            string x = str;
+            sort(x.begin(), x.end());
+            anagrams_[x].push_back(str);
         }
-        vector<vector<string>> returner;
-        for (auto x = anagrams.begin(); x != anagrams.end(); x++) {
-            vector<string> copier;
-            for (string str : x->second) {
-                copier.push_back(str);
-            }
-            returner.push_back(copier);
+        for (auto x = anagrams_.begin(); x != anagrams_.end(); x++)  {
+            result.push_back(x->second);
         }
-        return returner;
+        return result;
     }
-
-
-    //definiton of an anagram are two strings having the same length and having the same frequency of each letter
 };
