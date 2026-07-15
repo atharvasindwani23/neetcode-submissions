85129e23c027a8cb51ad6f9d5694c8ee8b80a45e
Atharva Sindwani <144984824+atharvasindwani23@users.noreply.github.com>
2026-07-14 17:28:41 -0700

    Update: climbing-stairs - submission-1

--- Data Structures & Algorithms/climbing-stairs/submission-1.cpp
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 1) {
-            return 1;
+        vector<int>dp (n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
         }
-        if (n == 2) {
-            return 2;
-        }
-        return climbStairs(n - 1) + climbStairs(n - 2);
+        return dp[n];
     }
+
+    //think about this by breaking the puzzle down n pieces, to reach stair n all u need to do is reach stair n - 1 or stair n - 2
+    //thats our whole dp logic.
 };
